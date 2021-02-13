b4e8ba19f16726da99214b7892787a1b9f040d7c
Akhil <akhilrao2797@gmail.com>
2021-02-13 22:14:07 +0530

    Jwt token generated

--- src/main/java/com/akhilrao2797/invest/security/JwtFilter.java
+++ src/main/java/com/akhilrao2797/invest/security/JwtFilter.java
@@ -29,7 +29,6 @@ public class JwtFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
 
         String authorizationHeader = httpServletRequest.getHeader("Authorization");
-
         String token = null;
         String userName = null;
 
