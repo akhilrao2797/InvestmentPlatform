0dbdf6c0147bc9c42d67af1b33fa8a9e15b09ec1
Akhil <akhilrao2797@gmail.com>
2021-02-15 20:04:12 +0530

    change in authority

--- src/main/java/com/akhilrao2797/invest/services/CustomUserDetailService.java
+++ src/main/java/com/akhilrao2797/invest/services/CustomUserDetailService.java
@@ -5,6 +5,8 @@ import com.akhilrao2797.invest.respository.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
@@ -22,6 +24,7 @@ public class CustomUserDetailService implements UserDetailsService {
     public UserDetails loadUserByUsername(String name) {
         LOG.info("Entered UserDetails.loadUserByUsername");
         User user = userRepository.findByName(name);
-        return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(), new ArrayList<>());
+        return new org.springframework.security.core.userdetails.User(user.getName(), user.getPassword(),
+                new ArrayList<>(new SimpleGrantedAuthority(user.getRole().toString())));
     }
 }
